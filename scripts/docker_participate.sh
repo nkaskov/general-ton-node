adb113136a1a2db1ec5b51e21e2eaa04f7ac110c
Battlelore21 <Battlelore21@protonmail.com>
2020-06-13 08:03:22 +0300

    participate bug fix

--- scripts/docker_participate.sh
+++ scripts/docker_participate.sh
@@ -10,5 +10,6 @@ fi
 docker exec -t $DOCKER_NAME bash -c "wget $CONFIG -O /var/ton-work/db/my-ton-global.config.json >> /var/ton-work/logs/participate.txt 2>&1"
 
 docker exec -t $DOCKER_NAME bash -c "cd /var/ton-work/contracts && reap.sh >> /var/ton-work/logs/reap.txt 2>&1"
+sleep 60
 docker exec -t $DOCKER_NAME bash -c "cd /var/ton-work/contracts && participate.sh >> /var/ton-work/logs/participate.txt 2>&1"
 
